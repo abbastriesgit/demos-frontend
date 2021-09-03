c546229af555c87808ad373f34fed7bda05dd4f6
abbas.al <abbas.al@media.net>
2021-09-04 00:06:57 +0530

    Added heroku urls

--- src/constants/urls.jsx
+++ src/constants/urls.jsx
@@ -1,12 +1,14 @@
 let urls = {
     "ttt":{
         "f":{
-            "base":"http://localhost:3000",
+            // "base":"http://localhost:3000",
+            "base":"https://abbas-ali.herokuapp.com",
             "join":"/demo/adv-tic-tac-toe/join",
             "home":"/demo/adv-tic-tac-toe"
         },
         "b":{
-            "base":"http://localhost:8080",
+            // "base":"http://localhost:8080",
+            "base":"https://abbas-ali-backend.herokuapp.com",
             "create": "/create",
             "join":"/join",
             "state":"/state",
