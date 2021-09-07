e228d7abfd9f981c6bd45f33646f6adc403ee857
abbas.al <abbas.al@media.net>
2021-09-08 02:12:46 +0530

    Added link to TTT folder

--- src/constants/urls.jsx
+++ src/constants/urls.jsx
@@ -4,7 +4,7 @@ let urls = {
     "email":"mailto: abbasali.bits@gmail.com",
     "genetic":"https://youtu.be/tV0CXsi9VH4?t=36",
     "ttt":{
-        "github":"https://github.com/abbastriesgit/demos-frontend/tree/master/src",
+        "github":"https://github.com/abbastriesgit/demos-frontend/tree/master/src/adv_tictactoe",
         "f":{
             // "base":"http://localhost:3000",
             "base":"https://abbas-ali.herokuapp.com",
